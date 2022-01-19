ef67380ad244cb9e337d84239aed49bba0ed65db
ian_otieno <ian.otieno@student.moringaschol.com>
2022-01-19 21:13:08 +0300

    add javascript function for budjet and edit

--- JS/user.js
+++ JS/user.js
@@ -14,3 +14,23 @@ var del = (element) => {
     document.getElementById(`exitem${element}`).innerText = "";
     document.getElementById(`icon${element}`).innerHTML = "";
 }
+//function for budget button
+function budgetclick() {
+    Name += Number(document.getElementById('budget').value);
+    document.getElementById('budjetprice').innerText = Name;
+    balamount += Number(document.getElementById('budget').value);
+    document.getElementById('balanceprice').innerText = balamount;
+    document.getElementById('budget').value = "";
+}
+// Function for editing budget
+var edit = (element) => {
+    examount -= Number(document.getElementById(`expenvl${element}`).innerText);
+    document.getElementById('expenseprice').innerText = examount;
+    balamount += Number(document.getElementById(`expenvl${element}`).innerText);
+    document.getElementById('balanceprice').innerText = balamount;
+    document.getElementById("expense").value = document.getElementById(`exitem${element}`).innerText;
+    document.getElementById("expenseamount").value = document.getElementById(`expenvl${element}`).innerText;
+    document.getElementById(`vl${element}`).innerText = "";
+    document.getElementById(`exitem${element}`).innerText = "";
+    document.getElementById(`icon${element}`).innerHTML = "";
+}
