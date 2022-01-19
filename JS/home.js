f38c2de8698ecb883cfac55b0a85b578d8b1f841
Hezron-tech <kiprophez@gmail.com>
2022-01-19 16:46:05 +0300

    adding funciton validates

---
+++ JS/home.js
@@ -0,0 +1,12 @@
+function validate(){
+    var username = document.getElementById("uname")
+    var password = document.getElementById("pass")
+
+    if (username.value=="" || password.value==""){
+        alert("Kindly fill the details");
+ 
+    }
+    else{
+        
+    }
+}
